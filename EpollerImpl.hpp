de1dd09ab6c28a8258a3a6fa31b682d9355a353e
GavinHolly <574752285@qq.com>
2015-10-13 16:16:25 +0800

    Create EpollerImpl.hpp

---
+++ EpollerImpl.hpp
@@ -0,0 +1,14 @@
+/**
+2  *
+3  *  File Name: EpollerImpl.hpp
+4  *  brief:
+5  *  Create Time: 2015年10月12日 23:40:05
+6  *  Author Gavin
+7  */                                                                                                   
+#ifndef EPOLLERIMPL_H
+#define EPOLLERIMPL_H
+class EpollerImpl
+{
+
+}
+#endif
